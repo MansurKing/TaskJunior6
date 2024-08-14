d09549f23ce3f26ca73786c2866798a473d86590
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-14 13:18:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TaskJunior6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystals;
+            int money;
+            int pricePerCrystal = 25;
+            bool isAbleToPay;
+
+            Console.WriteLine("Приветствую тебя, странник, в нашем большом МАГАЗИНЕ Кристаллов\nЗдесь ты можешь покупать различные кристаллы со всего мира и не только....");
+            Console.WriteLine($"Сегодня и только сейчас, именно для тебя, странник, ВСЕ кристаллы будут стоить по {pricePerCrystal} золота");
+            Console.Write("Сколько у вас золота: ");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько вам нужно кристаллов: ");
+            crystals = Convert.ToInt32(Console.ReadLine());
+
+            isAbleToPay = money >= crystals * pricePerCrystal;
+            crystals *= Convert.ToInt32(isAbleToPay);
+            money -= crystals * pricePerCrystal;
+
+            Console.WriteLine($"У вас в рукзаке {crystals} единицы кристаллов и {money} золота");
+        }
+    }
+}
